5e0ddd8831a4a34fe4af2741d22125d516b9e5b3
unknown <ibrahim.olawale13@gmail.com>
2022-01-16 11:35:39 +0100

    initial commit

---
+++ HelloRust/target/debug/HelloRust.d
@@ -0,0 +1,1 @@
+C:\Users\OLAWALE\ IBRAHIM\IdeaProjects\HelloRust\target\debug\HelloRust.exe: C:\Users\OLAWALE\ IBRAHIM\IdeaProjects\HelloRust\src\main.rs C:\Users\OLAWALE\ IBRAHIM\IdeaProjects\HelloRust\src\sh.rs
---
+++ HelloRust/target/debug/deps/HelloRust.d
@@ -0,0 +1,6 @@
+C:\Users\OLAWALE IBRAHIM\IdeaProjects\HelloRust\target\debug\deps\HelloRust.exe: src\main.rs src\sh.rs
+
+C:\Users\OLAWALE IBRAHIM\IdeaProjects\HelloRust\target\debug\deps\HelloRust.d: src\main.rs src\sh.rs
+
+src\main.rs:
+src\sh.rs:
